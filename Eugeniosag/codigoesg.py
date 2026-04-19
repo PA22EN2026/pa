b6a63b4381b90d36e8db15e4455a4ff8886be134
Eugeniosag <eugenio.sanchezg@uanl.edu.mx>
2026-04-19 15:03:21 -0600

    Se creo la clase Consultorio

--- Eugeniosag/codigoesg.py
+++ Eugeniosag/codigoesg.py
@@ -1,1 +1,9 @@
 print("soy eugenio")
+class Consultorio:
+    def __init__(self,id_consultorio,sucursal,piso,medico):
+        self.__id_consultorio=id_consultorio
+        self.__sucursal=sucursal
+        self.__piso=piso
+        self.__medico=medico
+        self.__medicamentos=[]
+        self.__pacientes=[]
