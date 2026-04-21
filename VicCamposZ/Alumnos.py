92d7267f4c435d568346e37d6f921682c22e86c9
Victoria Campos <camposvictoria777@gmail.com>
2026-04-20 18:55:19 -0600

    se agregaron atributos privados

--- VicCamposZ/Alumnos.py
+++ VicCamposZ/Alumnos.py
@@ -1,4 +1,9 @@
 print("VicCamposZ")
 
 class Alumnos: 
-    pass
+     def __init__(self, matricula, nombre, carrera, semestre, edad):
+        self.__matricula = matricula
+        self.__nombre = nombre
+        self.__carrera = carrera
+        self.__semestre = semestre
+        self.__edad = edad
