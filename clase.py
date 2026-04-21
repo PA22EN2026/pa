df24083d092711e62ce4b60b7dd20cc40f350603
camilardz19 <rodriguezsalinascamila19@gmail.com>
2026-04-20 19:52:48 -0600

    primer commit

---
+++ clase.py
@@ -0,0 +1,8 @@
+class Computadora:
+    def __init__(self, marca, modelo, ram, almacenamiento, precio):
+        self.__marca = marca
+        self.__modelo = modelo
+        self.__ram = ram
+        self.__almacenamiento = almacenamiento
+        self.__precio = precio
+        
