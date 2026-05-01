acb620ca7327d2b81d44e87f22527a49b0ed3fe8
Sebastián Barrón García <bgsebastian245@gmail.com>
2026-04-30 22:35:31 -0600

    Función 'menor' creada

--- SebasKZz/clasesebas.py
+++ SebasKZz/clasesebas.py
@@ -15,3 +15,11 @@ def mayor(self):
         return "El numero 2 es mayor"
     else:
         return "El numero 3 es mayor"
+
+def menor(self):
+    if self.__num1 < self.__num2 and self.__num1 < self.__num3:
+        return "El numero 1 es menor"
+    elif self.__num2 < self.__num1 and self.__num2 < self.__num3:
+        return "El numero 2 es menor"
+    else: 
+        return "El numero 3 es menor"
