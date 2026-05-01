df1c2a848549b8e02efff40611aa21dc265ce6e3
cami <rodriguezsalinascamila19@gmail.com>
2026-05-01 17:45:22 -0600

    agrege getters

--- camilardz19/evidencia3.py
+++ camilardz19/evidencia3.py
@@ -3,4 +3,14 @@ class Mi_Clase:
         self.__num1 = num1
         self.__num2 = num2
         self.__num3 = num3
+
+    def get_num1(self):
+        return self.__num1
+
+    def get_num2(self):
+        return self.__num2
+
+    def get_num3(self):
+        return self.__num3
+
       
