d86d592012c9412809bd7e642662bccc099b4836
leozpt1 <leozapata104@gmail.com>
2026-05-01 18:18:20 -0600

    añadí los getters y setters a la clase

--- leozpt1/evidencia_3.py
+++ leozpt1/evidencia_3.py
@@ -2,4 +2,22 @@ class Mi_Clase:
     def __init__(self, num1, num2, num3):
         self.__num1 = num1
         self.__num2 = num2
-        self.__num3 = num3 
+        self.__num3 = num3
+
+    def get_num1(self):
+        return self.__num1
+    
+    def get_num2(self):
+        return self.__num2
+    
+    def get_num3(self):
+        return self.__num3
+    
+    def set_num1(self,valor):
+        self.__num1 = valor
+
+    def set_num2(self,valor):
+        self.__num2 = valor
+
+    def set_num3(self,valor):
+        self.__num3 = valor
