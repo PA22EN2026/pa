43ad9d8794ace95d4b35496ad6abd5c76d2fae67
Ilce Monserrat Montoya <ilce.montoyas@uanl.edu.mx>
2026-05-02 15:17:59 -0600

    Se crean los getters y setters de los atributos privados

--- MMV1333-il/Mi_clase-il.py
+++ MMV1333-il/Mi_clase-il.py
@@ -3,3 +3,28 @@ class Mi_clase:
         self.__num1 = num1
         self.__num2 = num2
         self.__num3 = num3
+
+    @property
+    def num1(self):
+        return self.__num1
+    
+    @num1.setter
+    def num1(self,nuevo):
+        self.__num1 = nuevo
+
+    @property
+    def num2(self):
+        return self.__num2
+    
+    @num2.setter
+    def num2(self,nuevo):
+        self.__num2 = nuevo
+
+    @property
+    def num3(self):
+        return self.__num3
+    
+    @num3.setter
+    def num3(self,nuevo):
+        self.__num3 = nuevo
+
