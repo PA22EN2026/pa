4ce6c9d9e3cf91af436254c2c96c2db863ad8021
SofiaOrtiz996 <sofiapatricia093@gmail.com>
2026-05-02 17:59:05 -0600

    Se creo el metodo mayor

--- SofiaOrtiz996/Practica_2.py
+++ SofiaOrtiz996/Practica_2.py
@@ -6,3 +6,15 @@ class Mi_Clase:
 
     def sumar (self):
         return self.__num1+ self.__num2+self.__num3
+
+    def mayor(self):
+        if self.__num1>self.__num2:
+            if self.__num1>self.__num3:
+                num_mayor=self.__num1
+            else:
+                num_mayor=self.__num3
+        else:
+            if self.__num2>self.__num3:
+                num_mayor=self.__num2
+            else:
+                num_mayor=self.__num3
