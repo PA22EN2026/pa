5bb10ff1156b6ebd8637f439735ed97c462927fc
Ilce Monserrat Montoya <ilce.montoyas@uanl.edu.mx>
2026-05-02 15:56:51 -0600

    Se agrego la funcion menor, que determina que numero de los 3 es menor

--- MMV1333-il/Mi_clase-il.py
+++ MMV1333-il/Mi_clase-il.py
@@ -42,3 +42,12 @@ class Mi_clase:
             return print(f"El numero mayor es {self.__num2}")
         else:
             return print(f"El numero mayor es {self.__num3}")
+    
+    #funcion menor
+    def menor(self):
+        if self.__num1 < self.__num2 and self.__num1 < self.__num3:
+            return print(f"El numero menor es {self.__num1}")
+        elif self.__num2 < self.__num1 and self.__num2 < self.__num3:
+            return print(f"El numero menor es {self.__num2}")
+        else:
+            return print(f"El numero menor es {self.__num3}")
