938a7504eeefcdf826dacac07d0b4727370abd75
nicolas cabello <angie.cabello16@gmail.com>
2026-05-03 14:44:54 -0600

    agregamos atributos privados

--- angiebarron23/myclase.py
+++ angiebarron23/myclase.py
@@ -1,4 +1,10 @@
 # Archivo de la clase Mi_Clase 
 
 class Mi_Clase:
-    pass
+    pass 
+
+class Mi_Clase:
+    def __init__(self, num1, num2, num3):
+        self.__num1 = None
+        self.__num2 = None
+        self.__num3 = None
