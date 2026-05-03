f9523fc48f794254d396f469441c730eb0786135
Azael Candelaria <jair4azael@gmail.com>
2026-05-02 19:01:49 -0600

    Corregi la funcion MAYOR, ya esta completa (Me estoy equivocando con los commits)

--- Azael-CZ/Ev3_Practica2.py
+++ Azael-CZ/Ev3_Practica2.py
@@ -11,3 +11,7 @@ class Mi_clase:
     def mayor(self):
         if self.__num1 > self.__num2 and self.__num1 > self.__num3:
             print(f"El numero mayor es: {self.__num1}")
+        elif self.__num2 > self.__num2 and self.__num1 > self.__num3:
+            print(f"El numero mayor es: {self.__num2}")
+        elif self.__num3 > self.__num2 and self.__num1 > self.__num1:
+            print(f"El numero mayor es: {self.__num3}")
