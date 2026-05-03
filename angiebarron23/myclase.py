21e7321e802b51e52f2a03a719c902c4e4fbe1ae
nicolas cabello <angie.cabello16@gmail.com>
2026-05-03 15:07:44 -0600

    metodos iguales y concatenar

--- angiebarron23/myclase.py
+++ angiebarron23/myclase.py
@@ -18,3 +18,9 @@ class Mi_Clase:
 
     def menor(self):
         return min(self.__num1, self.__num2, self.__num3)
+    
+    def iguales(self):
+        return self.__num1 == self.__num2 == self.__num3
+    
+    def concatenar(self):
+        return str(self.__num1) + str(self.__num2) + str(self.__num3)
