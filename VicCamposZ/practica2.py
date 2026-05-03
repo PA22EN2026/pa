f8b8d454943bfd6d5550459dce8dcc6ee3722392
Victoria Campos <camposvictoria777@gmail.com>
2026-05-02 19:38:00 -0600

    metodo menor agregado

--- VicCamposZ/practica2.py
+++ VicCamposZ/practica2.py
@@ -14,3 +14,10 @@ class Numeros:
         else:
             return self.__num3
 
+      def menor(self):
+        if self.__num1 <= self.__num2 and self.__num1 <= self.__num3:
+            return self.__num1
+        elif self.__num2 <= self.__num1 and self.__num2 <= self.__num3:
+            return self.__num2
+        else:
+            return self.__num3
