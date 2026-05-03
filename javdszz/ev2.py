028854475228cb48606ac14b1b4eeabf870067db
javidszz <javirdz1020@gmail.com>
2026-05-03 17:35:05 -0600

    Se creo la clase y el metodo de suma

---
+++ javdszz/ev2.py
@@ -0,0 +1,9 @@
+class Mi_clase:
+    def __init__(self, num1, num2, num3):
+        self.__num1 = num1
+        self.__num2 = num2
+        self.__num3 = num3
+
+    def sumas(self):
+        suma = self.__num1 + self.__num2 + self.__num3
+        return suma 
