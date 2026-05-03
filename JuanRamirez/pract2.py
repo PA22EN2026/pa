deffb47f7222a8c8ae07ddc8060a7f64df8fd36a
JuanRamirez77 <carlozrmzz77@gmail.com>
2026-05-03 11:38:01 -0600

    Prueba de los metodos

--- JuanRamirez/pract2.py
+++ JuanRamirez/pract2.py
@@ -22,27 +22,44 @@ class Practica:
     def set_num3(self,num3):
         self.__num3=num3
 
-def sumar(self):
-    return self.__num1 + self.__num2 + self.__num3
+    def sumar(self):
+        return self.__num1 + self.__num2 + self.__num3
 
-def mayor(self):
-     if self._num1 >= self.num2 and self.num1 >= self._num3:
-            return self.__num1
-        elif self._num2 >= self.num1 and self.num2 >= self._num3:
+    def mayor(self):
+        if self.__num1 >= self.__num2 and self.__num1 >= self.__num3:
+                return self.__num1
+        elif self.__num2 >= self.__num1 and self.__num2 >= self.__num3:
             return self.__num2
         else:
             return self.__num3 
 
-def menor(self):
-        if self._num1 <= self.num2 and self.num1 <= self._num3:
+    def menor(self):
+        if self.__num1 <= self.__num2 and self.__num1 <= self.__num3:
             return self.__num1
-        elif self._num2 <= self.num1 and self.num2 <= self._num3:
+        elif self.__num2 <= self.__num1 and self.__num2 <= self.__num3:
             return self.__num2
         else:
             return self.__num3
 
-def iguales(self):
-    return self.__num1 == self.__num2 == self.__num3
+    def iguales(self):
+        return self.__num1 == self.__num2 == self.__num3
 
-def concatenar(self):
-    return str(self.__num1)+ str(self.__num2)+ str(self.__num3)     
+    def concatenar(self):
+        return str(self.__num1)+ str(self.__num2)+ str(self.__num3)  
+
+
+numeros=Practica(1,2,3)
+
+numeros.set_num1(77)
+numeros.set_num2(22)
+numeros.set_num3(66)
+
+print(f"La suma de los numeros es:",numeros.sumar())
+print(f"El numero mayor es:", numeros.mayor())
+print(f"El numero menor es:", numeros.menor())
+print(f"Los numeros son iguales:",numeros.iguales())
+print(f"Concatenacion de los numeros:",numeros.concatenar())
+
+
+
+
