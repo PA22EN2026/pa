84428f8c0d50831d4aac376802bb141c8f57f10a
Azael Candelaria <jair4azael@gmail.com>
2026-05-02 23:01:23 -0600

    Agregue los GETS y SETS

--- Azael-CZ/Ev3_Practica2.py
+++ Azael-CZ/Ev3_Practica2.py
@@ -29,7 +29,22 @@ class Mi_clase:
             print("Los tres numeros son iguales")
         else:
             print("Los tres numeros no son iguales")
-            
+
     def concatenar(self):
         num = str(self.__num1) + str(self.__num2) + str(self.__num3)
         print(num)
+
+    def get_num1 (self):
+        return self.__num1
+    def set_num1 (self, num1):
+        self.__num1 = num1
+
+    def get_num2 (self):
+        return self.__num2
+    def set_num2 (self, num2):
+        self.__num2 = num2
+
+    def get_num3 (self):
+        return self.__num3
+    def set_num3 (self, num3):
+        self.__num3 = num3
