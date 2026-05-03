9ff54421beff41c7c008c6e0e6a08eeaae8520fa
SofiaOrtiz996 <sofiapatricia093@gmail.com>
2026-05-02 19:08:51 -0600

    Se agrego el menu

--- SofiaOrtiz996/Practica_2.py
+++ SofiaOrtiz996/Practica_2.py
@@ -41,3 +41,17 @@ class Mi_Clase:
         return str(self.__num1)+str(self.__num2)+str(self.__num3)
 
 numeros=Mi_Clase(10,10,10)
+
+print("***Menu***")
+print("1.-Sumar/2.-mayor/3.- Menor/4.-Iguales/5.-concatenar")
+opcion= int(input("Que opcion elige"))
+if opcion==1:
+    print("Suma:", numeros.sumar())
+elif opcion ==2:
+    print("Mayor:", numeros.mayor())
+elif opcion==3:
+    print("Menor:", numeros.menor())
+elif opcion==4:
+    print("¿Son iguales?:", numeros.iguales())
+elif opcion==5:
+    print("Concatenación:", numeros.concatenar())
