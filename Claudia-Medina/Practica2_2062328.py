6fe563cc3cbafbb43b5a103684d84d9cb621d285
Claudia-Medina-UNI <claudia.medinap@uanl.edu.mx>
2026-05-02 23:56:47 -0600

    Añadiendo toques finales corrigiendo mas errores y decorando un poco el codigo

--- Claudia-Medina/Practica2_2062328.py
+++ Claudia-Medina/Practica2_2062328.py
@@ -33,12 +33,16 @@ numero2=int(input("Ingrese el numero 2: "))
 numero3=int(input("Ingrese el numero 3: "))
 
 Uso_deMIobjeto=Mi_Clase(numero1,numero2,numero3)
-
-print(f"Suma total: {Uso_deMIobjeto.sumar()}")
-print(f"Numero mayor: {Uso_deMIobjeto.mayor()}")
-print(f"Numero menor: {Uso_deMIobjeto.menor()}")
-print(f"¿Hay numero iguales?: {Uso_deMIobjeto.iguales()}")
-print(f"Concatenacion: {Uso_deMIobjeto.concatenar()}")
+print("⊱──────────────────────────────────────────⊰")
+print("           ✧ I M P R E S I O N ✧           ")
+print("⊱──────────────────────────────────────────⊰")
+print(f" ⫸  Suma total: {Uso_deMIobjeto.sumar()}")
+print(f" ⫸  Numero mayor: {Uso_deMIobjeto.mayor()}")
+print(f" ⫸  Numero menor: {Uso_deMIobjeto.menor()}")
+print(f" ⫸  ¿Hay numero iguales?: {Uso_deMIobjeto.iguales()}")
+print(f" ⫸  Concatenacion: {Uso_deMIobjeto.concatenar()}")
+print("⊱──────────────────────────────────────────⊰")
+print("                ⊹ ⊱ ✿ ⊰ ⊹                ")
 
     
         
