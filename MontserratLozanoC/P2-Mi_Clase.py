f3b296f9fa8d5d420043fca0f8a800ed113dd8c5
André Lozano Chapa <montserrat.lozanoc@uanl.edu.mx>
2026-05-03 20:21:04 -0600

    Agregar metodo iguales

--- MontserratLozanoC/P2-Mi_Clase.py
+++ MontserratLozanoC/P2-Mi_Clase.py
@@ -23,3 +23,6 @@ class Mi_Clase:
             return self.__num2
         else:
             return self.__num3
+        
+    def iguales(self):
+        return self.__num1 == self.__num2 == self.__num3
