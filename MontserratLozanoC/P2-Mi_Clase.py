eac9fe4b2f5fb0006e3df3350c831b68448591b1
André Lozano Chapa <montserrat.lozanoc@uanl.edu.mx>
2026-05-03 20:12:58 -0600

    Agregar metodo sumar

--- MontserratLozanoC/P2-Mi_Clase.py
+++ MontserratLozanoC/P2-Mi_Clase.py
@@ -4,3 +4,6 @@ class Mi_Clase:
         self.__num1 = num1
         self.__num2 = num2
         self.__num3 = num3
+        
+    def sumar(self):
+        return self.__num1 + self.__num2 + self.__num3
